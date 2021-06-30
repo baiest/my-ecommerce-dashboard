07ea00ddd5ebc86ddd2a37ca805e14dbb8dfe85c
baiest <juanballesteros2001@gmail.com>
2021-06-30 18:09:29 -0500

    Form product

--- src/components/Forms/FormProduct.jsx
+++ src/components/Forms/FormProduct.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import axios from 'axios'
 import '../../assets/css/FormProduct.css'
+import { API_PRODUCT_NEW, API_CATEGORIES }from '../../providers/api'
 class FormProduct extends React.Component{
     constructor(props){
         super(props)
         this.state = {
             update: this.props.update || false,
+            categories: [],
             form: {
                 product_id: {
                     type: 'text',
@@ -26,12 +29,29 @@ class FormProduct extends React.Component{
                     value: 0,
                     placeholder: 'Price'
                 },
-                created: this.state?.update ? '' : new Date(),
-                updated: new Date()
+                category_id: []
             }
         }
+        this.source = axios.CancelToken.source();
+
         this.handleInput = this.handleInput.bind(this)
+        this.addCategory = this.addCategory.bind(this)
         this.save = this.save.bind(this)
+    }
+
+    async componentDidMount(){
+        try{
+            const response = await axios.get(API_CATEGORIES,  {
+                cancelToken: this.source.token
+            })
+            console.log(response.data)
+            this.setState({categories: response.data, loading: false})
+
+        }catch(error){
+            if(!axios.isCancel(error) && error.code !== 'ECONNABORTED'){
+                this.setState({error: error.message, loading: false})
+            }
+        }
     }
     
     handleInput(e){
@@ -47,8 +67,39 @@ class FormProduct extends React.Component{
         this.setState(input)
     }
 
-    save(){
-        console.log(this.state.form)
+    addCategory(category){
+        const { form } = this.state
+        const { category_id } = form
+        let index_category = category_id.findIndex(c => c.category_id === category.category_id)
+        console.log(index_category, )
+        index_category > -1
+        ? category_id.splice(index_category, 1)
+        : category_id.push(category)
+
+        this.setState({ form: {
+            ...form, 
+            category_id: category_id.sort((a, b) => a.category_name > b.category_name? 1 : -1)
+        }})
+    }
+
+    async save(){
+        const { form } = {...this.state}
+        const form_keys = Object.keys(form)
+        const form_normalize = {}
+        form_keys.map(i => (
+            form_normalize[i] = form[i].value
+        ))
+
+        //this.validate()
+        try {
+            const response = await axios.post(
+                API_PRODUCT_NEW, 
+                form_normalize,
+                )
+            console.log(response)
+        } catch (error) {
+            console.log(error.message)
+        }
     }
 
     render(){
@@ -57,30 +108,51 @@ class FormProduct extends React.Component{
         
         return(
             <form className='form__product'>
-                {
-                    form_keys.map(i => {
-                        const input = () => (
-                        <input
-                        key={i} 
-                        id={i} 
-                        type="text"
-                        placeholder={form[i].placeholder}
-                        onChange={this.handleInput}/>)
-
-                        return (
-                        form[i].type &&
-                        <React.Fragment key={i}> 
-                            {form[i].type === 'number' 
-                            ? <div className="form__input">
-                                {input()}
-                                <button type="button">+</button>
-                                <button type="button">-</button>
-                            </div>
-                            : input()
+                <div className="form__product-fields">
+                    {
+                        form_keys.map(i => {
+                            const input = () => (
+                            <input
+                            key={i} 
+                            id={i} 
+                            type="text"
+                            placeholder={form[i].placeholder}
+                            onChange={this.handleInput}/>)
+                            
+                            if(form[i].type){
+                                if(form[i].type === "number"){
+                                    return <div  key={i} className="form__input">
+                                        {input()}
+                                        <button type="button">+</button>
+                                        <button type="button">-</button>
+                                    </div>
+                                }
+                                else{
+                                    return input()
+                                }
                             }
-                        </React.Fragment> 
-                    )})
-                }
+                            return null
+                        })
+                    }
+                </div>
+                <input type="file" onChange={this.onFileChange} />
+                <div className="form__product-categories">
+                    <div className="form__categories">
+                        { this.state.categories.map(c => {
+                            return(
+                                <div key={c.category_id}>
+                                    <input className="form__category-input" id={c.category_id} type="checkbox" onChange={() => this.addCategory(c)} />
+                                    <label className="form__category-name" htmlFor={c.category_id}>{c.category_name}</label>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <ul className="form__categories-list">
+                        {
+                            form.category_id.map(c => <li key={c.category_id}>{c.category_name}</li>)
+                        }
+                    </ul>
+                </div>
                 <div className="form__options">
                     <button type="button" onClick={this.save}>Guardar</button>
                     <button type="button">Cancelar</button>
---
+++ src/providers/api.js
@@ -0,0 +1,11 @@
+const API_URL = 'http://192.168.1.55:5000/api'
+
+//PRODUCTS
+export const API_PRODUCTS = `${API_URL}/products`
+export const API_PRODUCT_ID = (id) => `${API_URL}/products/${id}`
+export const API_PRODUCTS_CATEGORY = (id) => `${API_URL}/products/category/${id}`
+export const API_PRODUCT_IMAGE = (id, name_image) => `${API_URL}/products/img/${id}${name_image ? `/${name_image}` : ''}`
+export const API_PRODUCT_NEW = `${API_URL}/products/new`
+
+//CATEGORIES
+export const API_CATEGORIES = `${API_URL}/categories`
